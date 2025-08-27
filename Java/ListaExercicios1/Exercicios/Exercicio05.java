602fbc5b45d0ea547f5d9f68aea93dfea01185c8
Arthur Abreu <arthur.h.a@hotmail.com>
2025-08-26 22:13:12 -0300

    aula de desenvolvimento web exercicios progresso

---
+++ Java/ListaExercicios1/Exercicios/Exercicio05.java
@@ -0,0 +1,13 @@
+package ListaExercicios1.Exercicios;
+
+public class Exercicio05 {
+    //5. Crie um programa que receba o ano e mês de nascimento da pessoa (ambos em numéricos) e indique se ela poderá votar esse ano.
+
+    public static void main(String[]args){
+        try{
+            System.out.println("Insira o dia do nascimento(xx)/xx/xxxx: ");
+
+    }catch(Exception e){
+
+    }
+}
