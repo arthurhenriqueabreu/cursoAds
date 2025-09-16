79b1f52be6228c6d2cfc0e0bfd5f880ad5273da5
Arthur Abreu <arthur.h.a@hotmail.com>
2025-09-15 22:07:53 -0300

    início do projeto, commit com a finalidade de salvar progresso para uso remoto

---
+++ Java/AtividadeFinal/quaseTudoGostoso.java
@@ -0,0 +1,17 @@
+public class quaseTudoGostoso {
+    public static void main(String[] args) {
+        int escolha = 0;
+        do{
+        
+            System.out.println("-----------------------------------------");
+            System.out.println("Bem vindo ao sistema Quase Tudo Gostoso!");
+            System.out.println("-----------------------------------------");
+            System.out.println("[1]Visualizar receitas disponíveis");
+            System.out.println("[2]Adicionar uma receita");
+            System.out.println("[3]Editar uma receita");
+            System.out.println("[4]Deletar uma receita");
+            System.out.println("[5]Mostrar detalhes da receita");
+            System.out.println("[6]Sair");
+        }while(escolha !=6);
+    }
+}
